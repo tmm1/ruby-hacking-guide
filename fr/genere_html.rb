2d3434edaca445464a5a88909eb214f1ed91fe00
scritch <scritch@2ba632a7-620d-0410-bd84-d74392fff1da>
2006-04-14 21:51:38 +0000

    Translation of chapter 2 in French by Jean-Denis VAUGUET
    git-svn-id: http://rhg.rubyforge.org/svn@58 2ba632a7-620d-0410-bd84-d74392fff1da

--- fr/genere_html.rb
+++ fr/genere_html.rb
@@ -11,8 +11,8 @@ FOOTER = <<EOS
 <hr>
 
 L'oeuvre originale est Copyright &copy; 2002 - 2004 Minero AOKI.<br>
-Traduction par $tag(translated by)$<br>
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/"><img alt="Creative Commons License" border="0" src="images/somerights20.png"/></a><br/>Cette oeuvre est sous la <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/">Licence Creative Commons Attribution-NonCommercial-ShareAlike2.5</a>.
+Traduction $tag(translated by)$<br>
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/"><img alt="Creative Commons License" border="0" src="images/somerights20.fr.png"/></a><br/>Cette oeuvre est sous la <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/deed.fr">Licence Creative Commons Attribution-NonCommercial-ShareAlike2.5</a>.
 
 </body>
 </html>
@@ -21,6 +21,6 @@ EOS
 $tags['generation day'] = Time.now.strftime("%d/%m/%Y")
 $tags['language'] = 'fr'
 
-TranslatedByRE = /^Traduit par (.+)$/
+TranslatedByRE = /^Traduction (.+)$/
 
 make(*ARGV) if __FILE__ == $0
